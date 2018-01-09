a6d46bbed89ad6256b187ba72f59411249108856
Harious <harious@HariousdeiMac.local>
2018-01-09 13:40:59 +0800

    init

---
+++ RACDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RACDemo
+//
+//  Created by Harious on 2018/1/9.
+//  Copyright © 2018年 zzh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
